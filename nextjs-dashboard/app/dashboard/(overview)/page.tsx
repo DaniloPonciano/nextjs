fa9c9c5a77cb013656dd149d6fc484395cc6d7bb
DaniloPonciano <danilo_ponciano@estudante.sc.senai.br>
2024-12-04 23:40:49 -0300

    Atualização do projeto, criação dos invoices

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -1,16 +1,16 @@
-import { CardWrapper } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {
   fetchCardData,
-  fetchLatestInvoices,
 } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+
  
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
+  
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -29,10 +29,9 @@ export default async function Page() {
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-      <Suspense fallback={<RevenueChartSkeleton />}>
+        <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
-        </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        </Suspense>       
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
